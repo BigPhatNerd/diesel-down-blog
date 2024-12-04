04fc2aab1544990553c9e4df387f3495bd9d2d09
BigPhatNerd <wilsonhorrell@gmail.com>
2024-12-03 20:21:43 -0600

    fdalk;

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -381,11 +381,8 @@ export interface ApiBlogPostBlogPost extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
+    comment: Schema.Attribute.Relation<'oneToOne', 'api::comment.comment'>;
     comments: Schema.Attribute.Relation<'oneToMany', 'api::comment.comment'>;
-    comments_list: Schema.Attribute.Relation<
-      'oneToMany',
-      'api::comment.comment'
-    >;
     Content: Schema.Attribute.RichText;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
@@ -418,7 +415,7 @@ export interface ApiCommentComment extends Struct.CollectionTypeSchema {
   attributes: {
     Author: Schema.Attribute.String;
     blog_post: Schema.Attribute.Relation<
-      'manyToOne',
+      'oneToOne',
       'api::blog-post.blog-post'
     >;
     Content: Schema.Attribute.RichText;
